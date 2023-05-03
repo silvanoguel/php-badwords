be54e6160754a450bf64ce0a3b5f88a33c8cea21
silvanoguel <silvanoguel@msn.com>
2023-05-03 14:01:28 +0200

    add:textarea, input and button

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>php badwords</title>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>php badword</title>
 </head>
 <body>
+<form method="GET" action="result.php">
+
+    <textarea name="paragraph" id="paragraph" ></textarea>
+    <input type="text" name="badword" id="badword" placeholder="please write a badword">
     
+    <button type="submit">SEND</button>
+
+</form>
 </body>
 </html>
